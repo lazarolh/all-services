d3e026f39784948b906c60fe3fae5a5bdbdf2528
lazarolh <100285569+lazarolh@users.noreply.github.com>
2025-05-24 13:27:31 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,4 +1,19 @@
---Este script 
+<!-- 
+ * Nombre del programa: script.sql
+ * ¿Para que sirve?
+ * Este script tiene la funcionalidad de crear las tablas necesarias en la base de datos de supabase, con tecnología postgres, que son necesarias 
+  * para la gestión de la información manejada en la aplicación
+  
+ * Autor: Erika Daniela Martinez Villa, 
+          Lázaro López Hernández, 
+          Ricardo Parra Bonilla, 
+          Ángel de Jesús Callejas Graillet,
+          Jesús Alberto Apolinar Hermenegildo
+ * 
+ * Fecha de creacion: 8 de abril del 2025
+ * Fecha de entrega: 24 de mayo del 2025
+ */ 
+-->
 create table profiles (
   id uuid primary key references auth.users on delete cascade,
   username text,
